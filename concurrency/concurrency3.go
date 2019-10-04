eec7952add3133852f1e1028556d19ba31ea61b5
hadz333 <dhadzic@uw.edu>
2019-10-03 21:30:42 -0700

    Finished worker pools pattern

--- concurrency/concurrency3.go
+++ concurrency/concurrency3.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+// "worker pools" pattern - a queue of work to be done and multiple concurrent workers pulling items off the queue
+
+
 func main() {
-	fmt.Println("Hello")
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
 }
+
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n - 1) + fib(n - 2)
+} 
